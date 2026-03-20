5d598947fa50dee697cf0bc4a9bdf905f64a76c5
kelley <kelleycastillo@outlook.com>
2026-03-19 17:32:19 -0700

    finished mappractice

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,28 +1,48 @@
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
-
+    Map<String, Integer> myMap = new HashMap<>();
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
-
+    myMap.put("kelley", 30);
+    myMap.put("kate", 25);
+    myMap.put("Charlie", 35);
     // Get the value associated with a given key in the Map
-
+    int valueForBob = myMap.get("kate");
+    System.out.println("Value for key 'kate': " + valueForBob);
     // Find the size (number of key/value pairs) of the Map
+    int size = myMap.size();
+    System.out.println("Size of the map: " + size);
 
-    // Replace the value associated with a given key (the size of the Map shoukld not change)
+    // Replace the value associated with a given key (the size of the Map should not change)
+    myMap.put("kate", 26);
 
     // Check whether the Map contains a given key
+    boolean hasAlice = myMap.containsKey("Alice");
+    System.out.println("Map contains key 'Alice': " + hasAlice);
 
     // Check whether the Map contains a given value
+    boolean hasValue30 = myMap.containsValue(30);
+    System.out.println("Map contains value 30: " + hasValue30);
 
     // Iterate over the keys of the Map, printing each key
+    for (String key : myMap.keySet()) {
+      System.out.println("Key: " + key);
+    }
 
     // Iterate over the values of the map, printing each value
+    for (Integer value : myMap.values()) {
+      System.out.println("Value: " + value);
+    }
 
     // Iterate over the entries in the map, printing each key and value
+    for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+      System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+    }
 
     /*
      * Usage tip!
