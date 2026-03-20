e69e876d8291e243646623ef76622c8c1ecc5c6f
kelley <kelleycastillo@outlook.com>
2026-03-19 17:48:11 -0700

    finished w setpractice

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,17 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    Set<String> colors = new HashSet<>();
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    colors.add("red");
+    colors.add("green");
+    colors.add("blue");
 
     // Check whether the Set contains a given String
+    System.out.println(colors.contains("red"));
 
     // Remove an element from the Set
-
+    colors.remove("blue");
     // Get the size of the Set
+    System.out.println(colors.size());
 
     // Iterate over the elements of the Set, printing each one on a separate line
+    for (String color : colors) {
+      System.out.println(color);
+    }
 
     /*
      * Warning!
